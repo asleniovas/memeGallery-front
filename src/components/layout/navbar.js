2ba3a7b092a07f4c11e598923f8a26c31b21886d
asleniovas <asleniovas@gmail.com>
2019-09-14 15:45:25 +0300

    navbar styling

--- src/components/layout/navbar.js
+++ src/components/layout/navbar.js
@@ -1,20 +1,28 @@
 import React from 'react';
 
+//navbar component
 const NavBar = (props) => (
     
-    <nav className="navbar fixed-top navbar-light bg-light">
+    <nav className="navbar fixed-top navbar-dark" style={NavBarStyle}>
         
-        <i class="fas fa-plus fa-lg"></i> Add Meme
-        
+        <button type="button" className="btn btn-success">
+            <i className="fas fa-plus fa-lg"></i> Add Meme
+        </button>
 
-        <a className="navbar-brand ml-auto" href="https://getbootstrap.com/">
-             <i class="fab fa-bootstrap fa-lg"></i> 
+        <label className="navbar-brand ml-auto">powered by</label>
+        <a className="navbar-brand" href="https://getbootstrap.com/">
+             <i className="fab fa-bootstrap fa-lg"></i> 
         </a>
         <a className="navbar-brand" href="https://reactjs.org/">
-            <i class="fab fa-react fa-lg"></i>
+            <i className="fab fa-react fa-lg"></i>
         </a>
         
     </nav>
 )
+
+//styling
+const NavBarStyle = {
+    backgroundColor: "#563d7c",
+}
 
 export default NavBar;
